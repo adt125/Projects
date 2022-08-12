cf653c2d9d888dd0ea813611fd5e8cf499624fce
Aditya <aj@vk.com>
2022-08-12 16:33:24 +0530

    Change in Schedule'

--- Security_webcam.py
+++ Security_webcam.py
@@ -9,8 +9,6 @@ while True:
     face = face_cascade.detectMultiScale(gray, 1.3, 5)
     for x, y, w, h in face:
         img = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
-        # face_roi = frame[y:y+h, x:x+w]
-        # gray_roi = gray[y:y+h, x:x+w]
         time_stamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
         file_name = f'photos\\img-{time_stamp}.png'
         cv2.imwrite(file_name, original_frame)        
